5c17183b1df6e7cfdbc2327b3bca15bf061b91d7
Andrey Zuev <stepgt@mail.ru>
2022-11-01 09:57:44 +1000

    Added map polyfill && tests

--- polyfills.js
+++ polyfills.js
@@ -1,1 +1,18 @@
-module.exports = function () {}
+module.exports = function () {
+  if (!Array.prototype.mapPolyfill) {
+    Array.prototype.mapPolyfill = function (callback) {
+      if (!(this instanceof Array || this instanceof String)) {
+        throw new TypeError('Array.prototype.mapPolyfill was called on wrong type!');
+      }
+      if (typeof callback !== 'function') {
+        throw new TypeError(`Array.prototype.mapPolyfill ${callback} is not a function!`);
+      }
+      //
+      const arr = [];
+      for (let i = 0; i < this.length; i++) {
+        arr.push(callback(this[i], i, this));
+      }
+      return arr;
+    };
+  }
+};
--- test.js
+++ test.js
@@ -6,7 +6,39 @@ describe('Test polyfills', () => {
   before(() => {
     polyfills();
   });
-  it('Test1', () => {
-    
+  //
+  describe('Testing mapPolyfill', () => {
+    it('Check - wrong type', () => {
+      function badFn() {
+        Array.prototype.mapPolyfill.call(true, (value) => value);
+      }
+      expect(badFn).to.throw(TypeError);
+    });
+
+    it('Check - no callback', () => {
+      function badFn() {
+        [1, 2, 3].mapPolyfill();
+      }
+      expect(badFn).to.throw(TypeError);
+    });
+
+    it('Check - simple loop', () => {
+      expect([1, 2, 3, 4].mapPolyfill((item) => item + 10)).to.deep.equal(
+        [1, 2, 3, 4].map((item) => item + 10),
+      );
+    });
+
+    it('Check - string', () => {
+      expect(Array.prototype.mapPolyfill.call('string', (value) => `${value},`)).to.deep.equal(
+        Array.prototype.map.call('string', (value) => `${value},`),
+      );
+    });
+
+    it('Check - properties', () => {
+      const arr = [1, 2, 3, 4];
+      expect(arr.mapPolyfill((item, index, arr) => [item, index, arr])).to.deep.equal(
+        arr.map((item, index, arr) => [item, index, arr]),
+      );
+    });
   });
 });
