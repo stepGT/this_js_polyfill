d74fc70154071bde2dd292194d6ed1baf213852b
Andrey Zuev <stepgt@mail.ru>
2022-11-07 09:08:47 +1000

    Added reduce polyfill && tests

--- polyfills.js
+++ polyfills.js
@@ -15,4 +15,23 @@ module.exports = function () {
       return arr;
     };
   }
+  //
+  if (!Array.prototype.reducePolyfill) {
+    Array.prototype.reducePolyfill = function (callback, initValue) {
+      if (!(this instanceof Array || this instanceof String)) {
+        throw new TypeError('Array.prototype.reducePolyfill was called on wrong type!');
+      }
+      if (typeof callback !== 'function') {
+        throw new TypeError(`Array.prototype.reducePolyfill ${callback} is not a function!`);
+      }
+      //
+      let acc = arguments.length >= 2 ? initValue : this[0];
+      let start = arguments.length >= 2 ? 0 : 1;
+      //
+      for (let i = start; i < this.length; i++) {
+        acc = callback(acc, this[i], i, this);
+      }
+      return acc;
+    };
+  }
 };
--- test.js
+++ test.js
@@ -41,4 +41,63 @@ describe('Test polyfills', () => {
       );
     });
   });
+
+  describe('Testing reducePolyfill', () => {
+    it('Check - wrong type', () => {
+      function badFn() {
+        Array.prototype.reducePolyfill.call(true, (acc, value) => acc + value);
+      }
+      expect(badFn).to.throw(TypeError);
+    });
+
+    it('Check - no callback', () => {
+      function badFn() {
+        [1, 2, 3].reducePolyfill();
+      }
+      expect(badFn).to.throw(TypeError);
+    });
+
+    it('Check - sum', () => {
+      const arr = [1, 2, 3, 4];
+      expect(arr.reducePolyfill((acc, value) => acc + value)).to.equal(
+        arr.reduce((acc, value) => acc + value),
+      );
+    });
+
+    it('Check - string', () => {
+      expect(
+        Array.prototype.reducePolyfill.call(
+          'test',
+          (acc, value) => {
+            acc[value] = value + 1;
+            return acc;
+          },
+          {},
+        ),
+      ).to.deep.equal(
+        Array.prototype.reduce.call(
+          'test',
+          (acc, value) => {
+            acc[value] = value + 1;
+            return acc;
+          },
+          {},
+        ),
+      );
+    });
+
+    it('Check - properties', () => {
+      expect(
+        [1, 2, 3, 4].reducePolyfill((acc, value, index, arr) => {
+          acc.push([value, index, arr]);
+          return acc;
+        }, []),
+      ).to.deep.equal(
+        [1, 2, 3, 4].reduce((acc, value, index, arr) => {
+          acc.push([value, index, arr]);
+          return acc;
+        }, []),
+      );
+    });
+  });
 });
